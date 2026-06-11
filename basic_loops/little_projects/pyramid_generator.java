13fd398fb241161537d6392a74471525c610b744
Abbad Waseem <aw.abbadwaseem@gmail.com>
2026-06-11 21:19:19 +0530

    intial_pyramid
    draft of pyramid pattern generator

---
+++ basic_loops/little_projects/pyramid_generator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Random pyramid generator");
+      Scanner rn=new Scanner(System.in);
+      int random=rn.nextInt();
+      System.out.println("Please choose an option");
+      int a=rn.nextInt();
+      switch (a){
+       case 1 :
+          for (int i=random;i>=0;i--){
+            System.out.println("*".repeat(i));
+          }
+          break;
+
+        case 2:
+          for (int i=1;i<=random;i++){
+            System.out.println("*".repeat(i));
+          }
+          break;
+    //Yaha par case 3 edit karna hai ek new pattern banana hai
+        case 3:
+          for (int i=random;i>=0;i--){
+              System.out.println("*".repeat(i));
+            }
+          break;
+
+        default :
+          System.out.println("Please choose the correct option");
+      }
+      
+    }
+}
