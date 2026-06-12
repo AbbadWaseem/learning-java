5b2571f4a74e468561deb86d4cb02d4ecb5ff182
Abbad Waseem <aw.abbadwaseem@gmail.com>
2026-06-12 15:39:45 +0530

    Create Multiplication_table.java
    just added code for multiplication table

---
+++ basic_loops/arithmetic/Multiplication_table.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int num1=sc.nextInt();
+      int num2=1;
+      System.out.printf("Multiplication Table of %d :\n",num1);
+      while (num2<=10){
+       int num3=num1*num2;
+       System.out.printf("%d x %d = %d%n",num1,num2,num3);
+       num2++;
+      }
+    }
+}
