e2d58acc145d2874e32268f499421921ecffa7d6
Abbad Waseem <aw.abbadwaseem@gmail.com>
2026-06-13 17:36:00 +0530

    Unified related statements and increased code readability
    Grouped all validation and evaluation cases into a single if-else chain and removed redundant if statement and unified all print statements at the end to maximize code readability

--- basic_loops/little_projects/prime_number_check.java
+++ basic_loops/little_projects/prime_number_check.java
@@ -3,25 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
       Scanner rc=new Scanner(System.in);
-      
       int pr=rc.nextInt();
       boolean isPrme=true;
-
-      if (pr<=1){
-        System.out.println("Prime numbers are strictly defined as positive integers greater than 1.");
-        isPrme=false;
-      }
-
       for(int i=2;i<pr;i++){
         if (pr%i==0){
           isPrme=false;
-          System.out.println("This is not a prime number");
           break;
         }
       }
-
-      if (isPrme){
+      if (pr<=1){
+        System.out.println("Prime numbers are strictly defined as positive integers greater than 1.");
+      }
+      else if (isPrme){
         System.out.println("This is a prime number");
+      }
+      else {
+        System.out.println("This is not a prime number");
       }
     }
 }
