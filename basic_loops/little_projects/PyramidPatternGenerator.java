1c41f1701ed3337357e8533e77991f970f6fcd42
Abbad Waseem <aw.abbadwaseem@gmail.com>
2026-06-14 18:07:11 +0530

    Update and rename pyramid_pattern_generator.java to PyramidPatternGenerator.java
    updated file name and also changed public class main to public class file name

--- basic_loops/little_projects/PyramidPatternGenerator.java
+++ basic_loops/little_projects/PyramidPatternGenerator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class PyramidPatternGenerator {
     public static void main(String[] args) {
       System.out.println("Random pyramid generator");
       Scanner rn=new Scanner(System.in);
