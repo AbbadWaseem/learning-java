5bb0a53f942fb300553dc816928eae5aed723aca
Abbad Waseem <aw.abbadwaseem@gmail.com>
2026-06-14 18:06:24 +0530

    Update and rename prime_number_check.java to PrimeNumberCheck.java
    updated file name and also changed public class main to public class file name

--- basic_loops/little_projects/PrimeNumberCheck.java
+++ basic_loops/little_projects/PrimeNumberCheck.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class PrimeNumberCheck {
     public static void main(String[] args) {
       Scanner rc=new Scanner(System.in);
       int pr=rc.nextInt();
