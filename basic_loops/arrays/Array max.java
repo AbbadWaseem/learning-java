3da363d199e2d9d6e9dd1f64a3a255a318f0379e
Abbad Waseem <aw.abbadwaseem@gmail.com>
2026-06-21 18:38:53 +0530

    Update Array max.java
    Booleans that i added were useless and I forgot that the int maxium will update it if it found anything so I was basically just wasting line of code by writing if else statement
    I also added to find minimum number and fixed some grammar

--- basic_loops/arrays/Array max.java
+++ basic_loops/arrays/Array max.java
@@ -7,19 +7,22 @@ public class ArrayMax {
         { 65, -81, 02 }
       };
       int maxium=array[0][0];
-      boolean test=false;
+      int minion=array[0][0];
       for (int i=0;i<array.length;i++){
         for (int j=0;j<array[0].length;j++){
           System.out.print(array[i][j]+" ");
           if (array[i][j]>maxium){
-            test=true;
             maxium=array[i][j];
+          }
+          if(array[i][j]<minion){
+            minion=array[i][j];
           }
         }
         System.out.println();
       }
-      System.out.println(test? "Yes there is maximum number "+maxium : "Maxiumum number is "+maxium);
- 
+      System.out.println("Maxiumum number is "+maxium);
+      System.out.println("Minimum number is "+minion);
+
       float aw=16.11f;
     }
 }
