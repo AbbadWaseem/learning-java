6ed75b5a6e611be7c87e7ae3c4b205dfd867034f
Abbad Waseem <aw.abbadwaseem@gmail.com>
2026-06-21 23:17:06 +0530

    Update and rename Sort Array.java to Reverse Array.java

--- basic_loops/arrays/Reverse Array.java
+++ basic_loops/arrays/Reverse Array.java
@@ -1,14 +1,14 @@
 import java.util.Arrays;
-public class Main{
+public class ReverseArray{
   public static void main (String[] args){
     int [] arr={2,4,9,25,169};
-    System.out.println(Arrays.toString(arr));
+    System.out.println("This is your current array\n"+Arrays.toString(arr));
     for (int i=0;i<arr.length/2;i++){
       int temp=arr[i];
       arr[i]=arr[arr.length-1-i];
       arr[arr.length-1-i]=temp;
     }
-    System.out.println(Arrays.toString(arr));   
+    System.out.println("\nThis is your reversed array\n"+Arrays.toString(arr));   
      
     float aw=16.11f;
   }
